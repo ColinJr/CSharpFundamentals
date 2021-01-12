36f5af56be7c720646239c070b37ac7a302a62b8
colin detwiller <colindetwillerjr@gmail.com>
2021-01-12 10:27:06 -0500

    Retried exercize49

---
+++ C_Sharp_Fundementals/notes/excersize49.cs
@@ -0,0 +1,79 @@
+﻿//Note: for all these exercises, ignore input validation unless otherwise directed.Assume the user enters a value in the format that the program expects.For example, if the program expects the user to enter a number, don't worry about validating if the input is a number or not. When testing your program, simply enter a number.
+using System;
+
+namespace notes.ControlFlow
+{
+
+
+    //1- Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder. Display the count on the console.
+    public class Conditionals
+    {
+        public void Exercise1()
+        {
+            Console.Write("Enter a number between 1 to 10: ");
+            var input = Console.ReadLine();
+            var number = Convert.ToInt32(input);
+            if (number >= 1 && number <= 10)
+                Console.WriteLine("Valid");
+            else
+                Console.WriteLine("Invalid");
+        }
+
+
+        //2- Write a program and continuously ask the user to enter a number or "ok" to exit. Calculate the sum of all the previously entered numbers and display it on the console.
+        public void Exercise2()
+        {
+            Console.Write("Enter a number: ");
+            var number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter another number: ");
+            var number2 = Convert.ToInt32(Console.ReadLine());
+
+            var max = (number1 > number2) ? number1 : number2;
+            Console.WriteLine("Max is " + max);
+        }
+
+
+        //3- Write a program and ask the user to enter a number. Compute the factorial of the number and print it on the console. For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1 and display it as 5! = 120.
+
+        public void Exercise3()
+        {
+            Console.Write("Image width: ");
+            var width = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Image height: ");
+            var height = Convert.ToInt32(Console.ReadLine());
+
+            var orientation = width > height ? ImageOrientation.Landscape : ImageOrientation.Portrait;
+            Console.WriteLine("Image orientation is " + orientation);
+        }
+
+        public enum ImageOrientation
+        {
+            Landscape,
+            Portrait
+        }
+
+
+        //4- Write a program that picks a random number between 1 and 10. Give the user 4 chances to guess the number. If the user guesses the number, display “You won"; otherwise, display “You lost". (To make sure the program is behaving correctly, you can display the secret number on the console first.)
+
+        public void Exercise4()
+        {
+            Console.Write("What is the speed limit? ");
+            var speedLimit = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("What is the speed of this car? ");
+            var carSpeed = Convert.ToInt32(Console.ReadLine());
+
+            if (carSpeed < speedLimit)
+                Console.WriteLine("Ok");
+            else
+            {
+                const int kmPerDemeritPoint = 5;
+                var demeritPoints = (carSpeed - speedLimit) / kmPerDemeritPoint;
+                if (demeritPoints > 12)
+                    Console.WriteLine("License Suspended");
+                else
+                    Console.WriteLine("Demerit points: " + demeritPoints);
+            }
+        }
